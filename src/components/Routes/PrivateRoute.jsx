32e1da18a4e2a7c1b19aa47a012cb1a926efebac
Shahriar-Hossain-Alvi <shahriarhossainalvi@gmail.com>
2024-12-21 17:24:05 +0600

    updated privateRoute.jsx to redirect users based on their role

--- src/components/Routes/PrivateRoute.jsx
+++ src/components/Routes/PrivateRoute.jsx
@@ -3,16 +3,20 @@ import useAuth from "../Hooks/useAuth";
 import PropTypes from 'prop-types';
 import LoadingSpinner from "../Utilities/LoadingSpinner";
 
-const PrivateRoute = ({ children }) => {
+const PrivateRoute = ({ children, role }) => {
     const { loading, user } = useAuth();
     const location = useLocation()
     
 
     if (loading) return <LoadingSpinner />
 
-    if (user) return children;
+    if (!user) return <Navigate to="/signin" state={{ from: location }}></Navigate>
 
-    return <Navigate to="/signin" state={{ from: location }}></Navigate>
+
+    if (role && user.user_role !== role) return <Navigate to={`/${user.role}/dashboard`} />
+
+
+    return children;
 };
 
 PrivateRoute.propTypes = {
