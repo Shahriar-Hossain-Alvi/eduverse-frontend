09023889bc7c02a96ddd70cbb227fea7293cec12
Shahriar-Hossain-Alvi <shahriarhossainalvi@gmail.com>
2025-03-25 15:33:36 +0600

    removed some unnecessary codes

--- src/components/Shared/Admin&FacultyShared/EnrolledStudentsClassAttendanceForm.jsx
+++ src/components/Shared/Admin&FacultyShared/EnrolledStudentsClassAttendanceForm.jsx
@@ -9,11 +9,14 @@ import { FaMinus, FaPlus } from "react-icons/fa";
 import useAuth from "../../Hooks/useAuth";
 import toast from "react-hot-toast";
 import { CgSpinnerTwoAlt } from "react-icons/cg";
+import { MdClose } from "react-icons/md";
+import { handleError } from "../../Utilities/handleError";
 
 
 
 
 const EnrolledStudentsClassAttendanceForm = ({ course_id, class_id }) => {
+    console.log(class_id);
     const axiosSecure = useAxiosSecure();
     const { user } = useAuth();
 
@@ -22,10 +25,10 @@ const EnrolledStudentsClassAttendanceForm = ({ course_id, class_id }) => {
     const [formSubmissionLoading, setFormSubmissionLoading] = useState(false);
 
 
-    const { register, reset, formState: { errors }, handleSubmit } = useForm({});
+    const { reset, handleSubmit } = useForm();
 
     // fetch attendance data
-    const { data: studentsAttendanceRecord = {}, isPending, isError, error } = useQuery({
+    const { data: studentsAttendanceRecord = {}, isPending, isError, error, refetch } = useQuery({
         queryKey: [],
         queryFn: async () => {
             const res = await axiosSecure.get(`/classAttendance/${class_id}`);
@@ -45,7 +48,7 @@ const EnrolledStudentsClassAttendanceForm = ({ course_id, class_id }) => {
         },
         enabled: !!course_id // Runs only when course_id is available
     });
-    // console.log(studentListForAttendance);
+    console.log(studentsAttendanceRecord);
 
 
     // Initialize attendance data when student list is loaded 
@@ -92,10 +95,15 @@ const EnrolledStudentsClassAttendanceForm = ({ course_id, class_id }) => {
         });
     };
 
+
+    // save attendance button
     const handleAddClassAttendance = async () => {
         // Make sure all students have an attendance record
         if (attendanceData.length !== studentListForAttendance.length) {
-            toast.error("Please mark attendance for all students");
+            toast.error("Please mark attendance for all students", {
+                duration: 2500,
+                position: "top-center"
+            });
             return;
         }
 
@@ -103,14 +111,36 @@ const EnrolledStudentsClassAttendanceForm = ({ course_id, class_id }) => {
         const finalData = {
             class_id,
             created_by: user._id,
-            attendance_date: new Date().toISOString(),
+            attendance_date: new Date(),
             attendance_record: attendanceData
         };
 
-        console.log(finalData);
+        try {
+            setFormSubmissionLoading(true);
+            const res = await axiosSecure.post("/classAttendance", finalData);
 
-        // Submit data
-        // addAttendance(finalData);
+            console.log(res);
+
+            if (res.data.success) {
+                toast.success(res.data.message, {
+                    duration: 2500,
+                    position: "top-center"
+                })
+                reset();
+                setFormSubmissionLoading(false);
+                setShowAttendanceForm(false);
+                refetch();
+            }
+        } catch (error) {
+            handleError(error, "Failed to save class attendance");
+            reset();
+            setFormSubmissionLoading(false);
+            setShowAttendanceForm(false);
+            toast.error("Failed to save class attendance", {
+                duration: 2500,
+                position: "top-center"
+            })
+        }
     }
 
 
@@ -118,96 +148,100 @@ const EnrolledStudentsClassAttendanceForm = ({ course_id, class_id }) => {
         <div>
             <SectionHeading title="Class Attendance" />
 
-            {/* error message */}
-            {isError && <TanstackQueryErrorMessage errorMessage={error.message} />}
-
-            {studentListForAttendanceIsError && <TanstackQueryErrorMessage errorMessage={studentListForAttendanceError.message} />}
-
-
-
 
             {/* attendance form */}
-            <div className="mb-3">
-                {
-                    showAttendanceForm ?
-                        <button onClick={() => {
-                            setShowAttendanceForm(!showAttendanceForm);
-                            reset();
-                        }} className="btn btn-error text-white">
-                            <FaMinus />
-                            Cancel
-                        </button>
-                        :
-                        <button onClick={() => setShowAttendanceForm(!showAttendanceForm)} className="btn btn-success text-white">
-                            <FaPlus />
-                            Add Attendance
-                        </button>
+            <div>
+                {/* error message for attendance form */}
+                {studentListForAttendanceIsError && <TanstackQueryErrorMessage errorMessage={studentListForAttendanceError.message} />}
 
 
-                }
-            </div>
-            {
-                showAttendanceForm &&
-                <form onSubmit={handleSubmit(handleAddClassAttendance)}>
-                    {studentListForAttendance.map(singleStudent => <div key={singleStudent._id} className="border p-2 grid grid-cols-3 gap-2 items-center">
 
-                        {/* student name */}
-                        <label className="form-control w-full">
-                            <div className="label">
-                                <span className="label-text">Name</span>
-                            </div>
-                            <input className="input input-bordered w-full" readOnly defaultValue={`${singleStudent.users_id.first_name} ${singleStudent.users_id.last_name}`} />
-                        </label>
-
-
-                        {/* select attendance */}
-                        <label className="form-control w-full">
-                            <div className="label">
-                                <span className="label-text">Select Attendance</span>
-                            </div>
-                            <div>
-                                <select defaultValue="absent"
-                                    onChange={e => handleAttendanceChange(singleStudent.users_id._id, "is_present", e.target.value)}
-                                    className="select select-bordered w-full">
-                                    <option value="present" className="text-success font-bold">Present</option>
-
-                                    <option value="absent" className="text-error font-bold">Absent</option>
-
-                                    <option value="early leave" className="text-warning font-bold">Early Leave</option>
-
-                                    <option value="late" className="text-warning font-bold">Late</option>
-                                </select>
-                            </div>
-                        </label>
-
-
-                        {/* remarks */}
-                        <label className="form-control w-full max-w-xs">
-                            <div className="label">
-                                <span className="label-text">Remarks</span>
-                            </div>
-                            <div>
-                                <input type="text"
-                                    onChange={e => handleAttendanceChange(singleStudent.users_id._id, "remarks", e.target.value)}
-                                    placeholder="Type here" className="input input-bordered w-full" />
-                                {errors.attendanceRemarks && <p className="text-error text-sm  pl-3 pt-1 animate-pulse">{errors.attendanceRemarks.message}</p>}
-                            </div>
-                        </label>
-                    </div>)}
-
-                    {/* submit button */}
+                <div className="mb-3">
                     {
-                        formSubmissionLoading ?
-                            <button type="submit" className="btn btn-disabled btn-wide mt-3 text-white">
-                                <CgSpinnerTwoAlt className="animate-spin" />
+                        showAttendanceForm ?
+                            <button onClick={() => {
+                                setShowAttendanceForm(!showAttendanceForm);
+                                reset();
+                            }} className="btn btn-error text-white">
+                                <MdClose />
+                                Cancel
                             </button>
                             :
-                            <button type="submit" className="btn btn-success btn-wide mt-3 text-white">
-                                Save
+                            <button onClick={() => setShowAttendanceForm(!showAttendanceForm)} className="btn btn-success text-white">
+                                <FaPlus />
+                                Add Attendance
                             </button>
+
+
                     }
-                </form>
-            }
+                </div>
+                {
+                    showAttendanceForm &&
+                    <form onSubmit={handleSubmit(handleAddClassAttendance)}>
+                        {studentListForAttendance.map(singleStudent => <div key={singleStudent._id} className="border p-2 grid grid-cols-3 gap-2 items-center">
+
+                            {/* student name */}
+                            <label className="form-control w-full">
+                                <div className="label">
+                                    <span className="label-text">Name</span>
+                                </div>
+                                <input className="input input-bordered w-full" readOnly defaultValue={`${singleStudent.users_id.first_name} ${singleStudent.users_id.last_name}`} />
+                            </label>
+
+
+                            {/* select attendance */}
+                            <label className="form-control w-full">
+                                <div className="label">
+                                    <span className="label-text">Select Attendance</span>
+                                </div>
+                                <div>
+                                    <select defaultValue="absent"
+                                        onChange={e => handleAttendanceChange(singleStudent.users_id._id, "is_present", e.target.value)}
+                                        className="select select-bordered w-full">
+                                        <option value="present" className="text-success font-bold">Present</option>
+
+                                        <option value="absent" className="text-error font-bold">Absent</option>
+
+                                        <option value="early leave" className="text-warning font-bold">Early Leave</option>
+
+                                        <option value="late" className="text-warning font-bold">Late</option>
+                                    </select>
+                                </div>
+                            </label>
+
+
+                            {/* remarks */}
+                            <label className="form-control w-full max-w-xs">
+                                <div className="label">
+                                    <span className="label-text">Remarks</span>
+                                </div>
+                                <div>
+                                    <input type="text"
+                                        onChange={e => handleAttendanceChange(singleStudent.users_id._id, "remarks", e.target.value)}
+                                        placeholder="Type here" className="input input-bordered w-full" />
+                                </div>
+                            </label>
+                        </div>)}
+
+                        {/* submit button */}
+                        {
+                            formSubmissionLoading ?
+                                <button type="submit" className="btn btn-disabled btn-wide mt-3 text-white">
+                                    <CgSpinnerTwoAlt className="animate-spin" />
+                                </button>
+                                :
+                                <button type="submit" className="btn btn-success btn-wide mt-3 text-white">
+                                    Save
+                                </button>
+                        }
+                    </form>
+                }
+            </div>
+
+
+
+            {/* error message */}
+            {isError && <TanstackQueryErrorMessage errorMessage={error.message} />}
         </div>
     );
 };
