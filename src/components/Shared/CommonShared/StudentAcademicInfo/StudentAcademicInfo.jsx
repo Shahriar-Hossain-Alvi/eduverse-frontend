1f20bc3aabe4197389488465931e05412869ebe5
Shahriar-Hossain-Alvi <shahriarhossainalvi@gmail.com>
2025-04-26 19:53:49 +0600

    added pdf download option to download grades as pdf

--- src/components/Shared/CommonShared/StudentAcademicInfo/StudentAcademicInfo.jsx
+++ src/components/Shared/CommonShared/StudentAcademicInfo/StudentAcademicInfo.jsx
@@ -3,16 +3,30 @@ import { useQuery } from "@tanstack/react-query"
 import SectionHeading from "../../../Utilities/SectionHeading";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import StudentGradesTab from "./StudentGradesTab";
-import useAuth from "../../../Hooks/useAuth";
 
 
 
 const StudentAcademicInfo = () => {
-    const { user } = useAuth();
+    const { id } = useParams();
+    const axiosSecure = useAxiosSecure();
+
+    // fetch grades
+    const { data: studentGrades = [], isPending: studentGradesIsPending, isError: studentGradesIsError, error: studentGradesError } = useQuery({
+        queryKey: [],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/studentGrades/student/${id}`);
+
+            return res.data.data;
+        },
+        enabled: !!id
+    })
+
+    const studentBasicInfo = studentGrades[0]?.student_id || "Deleted/Unavailable User";
+
 
 
     return (
-        <div className="flex-1 p-3 md:p-8">
+        <div className="flex-1 p-3 md:p-8 overflow-hidden">
             <SectionHeading title="Academic Details" />
 
 
@@ -22,24 +36,20 @@ const StudentAcademicInfo = () => {
                     <h1>Overview of</h1>
                     <h2>Email Address</h2>
                     <h2>Phone Number</h2>
-                    {user?.user_role === "admin" && <h2>Username</h2>}
                 </div>
 
                 <div className="text-sm md:text-xl font-medium px-2">
                     <p>:</p>
                     <p>:</p>
                     <p>:</p>
-                    {user?.user_role === "admin" && <p>:</p>}
+
                 </div>
 
 
                 <div className="text-sm md:text-xl">
-                    <h1 className="uppercase">{user?.first_name || "Deleted"} {user?.last_name || "User"}</h1>
-                    <h2>{user?.email || "unavailable"}</h2>
-                    <h2>{user?.phone || "unavailable"}</h2>
-                    {
-                        user?.user_role === "admin" && <h2>{user?.user_name || "unavailable"}</h2>
-                    }
+                    <h1 className="uppercase">{studentBasicInfo?.first_name || "Deleted"} {studentBasicInfo?.last_name || "User"}</h1>
+                    <h2>{studentBasicInfo?.email || "unavailable"}</h2>
+                    <h2>{studentBasicInfo?.phone || "unavailable"}</h2>
                 </div>
             </div>
 
@@ -53,7 +63,11 @@ const StudentAcademicInfo = () => {
                 <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Grades" defaultChecked />
 
                 <div role="tabpanel" className="tab-content p-6 border-base-300 rounded-box overflow-hidden">
-                    <StudentGradesTab />
+                    <StudentGradesTab
+                        studentGrades={studentGrades} studentGradesIsPending={studentGradesIsPending}
+                        studentGradesIsError={studentGradesIsError}
+                        studentGradesError={studentGradesError}
+                    />
                 </div>
 
 
--- src/components/Shared/CommonShared/StudentAcademicInfo/StudentGradesTab.jsx
+++ src/components/Shared/CommonShared/StudentAcademicInfo/StudentGradesTab.jsx
@@ -1,80 +1,152 @@
-import { useParams } from "react-router";
-import useAxiosSecure from "../../../Hooks/useAxiosSecure";
-import { useQuery } from "@tanstack/react-query";
+import autoTable from "jspdf-autotable";
 import LoadingSpinner from "../../../Utilities/LoadingSpinner";
 import TanstackQueryErrorMessage from "../../../Utilities/TanstackQueryErrorMessage";
 import { format } from "date-fns";
+import PropTypes from 'prop-types';
+import jsPDF from "jspdf";
+import { FaDownload } from "react-icons/fa";
 
 
-const StudentGradesTab = () => {
-    const { id } = useParams();
-    const axiosSecure = useAxiosSecure();
 
-    // fetch grades
-    const { data: studentAcademicInfo = [], isPending, isError, error } = useQuery({
-        queryKey: [],
-        queryFn: async () => {
-            const res = await axiosSecure.get(`/studentGrades/student/${id}`);
 
-            return res.data.data;
-        },
-        enabled: !!id
-    })
+const StudentGradesTab = ({ studentGrades, studentGradesIsPending, studentGradesIsError, studentGradesError }) => {
 
-    console.log(studentAcademicInfo);
 
-    if (isPending) return <LoadingSpinner />
+
+    // create attendance pdf
+    const generatePDF = (gradesData) => {
+
+        const doc = new jsPDF();
+
+        // get student info
+        const gradesOf = gradesData[0]?.student_id;
+
+
+        // title
+        doc.setFontSize(30);
+        doc.text("Grades of completed courses", 45, 10);
+
+        // student name, email, phone
+        doc.setFontSize(16);
+        doc.text(`Grades of: ${gradesOf.first_name || "Deleted"} ${gradesOf.last_name || "User"}`, 60, 20);
+
+        doc.setFontSize(12);
+        doc.text(`Email: ${gradesOf.email || "Not Found"}`, 60, 27);
+
+        doc.setFontSize(12);
+        doc.text(`Phone: ${gradesOf.phone || "Not Found"}`, 60, 32);
+
+        // rows for attendance
+        const tableData = [];
+
+
+        gradesData.forEach((record) => {
+            const row = [
+                record.course_id.title,
+                format(new Date(record?.course_id?.start_date), "MMMM d, yyyy") + " - " + format(new Date(record?.course_id?.end_date), "MMMM d, yyyy")
+                ,
+                record.full_marks,
+                record.obtained_marks,
+                record.percentage.toFixed(2),
+                record.remarks,
+                record.faculty_id.first_name + " " + record.faculty_id.last_name,
+
+            ];
+            tableData.push(row);
+        });
+
+
+        // Define the columns for the table
+        autoTable(doc, {
+            head: [["Course Name", "Duration", "Full Marks", "Obtained Marks", "Percentage", "Remarks", "Graded By"]],
+            body: tableData,
+            startY: 50,
+            styles: {
+                fontSize: 10,
+                cellPadding: 2,
+            },
+        })
+
+        doc.save(`grades of ${gradesOf.first_name}_${gradesOf.last_name}.pdf`);
+    }
+
+
+    if (studentGradesIsPending) return <LoadingSpinner />
 
     return (
         <div>
-            Tab 1: Grades Table (download pdf)
 
-            {isError && <TanstackQueryErrorMessage errorMessage={error.message} />}
+            {studentGradesIsError && <TanstackQueryErrorMessage errorMessage={studentGradesError.message} />}
 
-            <div className="overflow-x-auto">
-                <table className="table table-xs md:table-sm">
-                    {/* head */}
-                    <thead>
-                        <tr>
-                            <th>No.</th>
-                            <th>Course name</th>
-                            <th>Duration</th>
-                            <th>Full Marks</th>
-                            <th>Obtained Marks</th>
-                            <th>Percentage</th>
-                            <th>Remarks</th>
-                            <th>Graded By</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            studentAcademicInfo.map((record, idx) =>
-                                <tr
-                                className="hover"
-                                key={record?._id}>
-                                    <th>{idx + 1}</th>
+            {
+                <div className="flex justify-end">
+                    <button
+                        disabled={studentGrades.length === 0}
+                        onClick={() => generatePDF(studentGrades)}
+                        className="btn text-white btn-sm flex btn-success"><FaDownload />
+                    </button>
+                </div>
+            }
 
-                                    <td>{record?.course_id?.title}</td>
+            {
+                studentGrades.length === 0 ?
+                    <p className="text-lg text-error font-medium text-center my-2">No Grades available to show</p>
+                    :
 
-                                    <td>{format(new Date(record?.course_id?.start_date), "MMMM d, yyyy")} - {format(new Date(record?.course_id?.end_date), "MMMM d, yyyy")}</td>
-
-                                    <td className="text-center">{record?.full_marks}</td>
-
-                                    <td className="text-center">{record?.obtained_marks}</td>
-
-                                    <td className="text-center">{record?.percentage.toFixed(2)}%</td>
-
-                                    <td className="text-center">{record?.remarks}</td>
-
-                                    <td className="text-center">{record?.faculty_id?.first_name} {record?.faculty_id?.last_name}</td>
+                    <div className="overflow-x-auto">
+                        <table className="table table-xs md:table-sm">
+                            {/* head */}
+                            <thead>
+                                <tr>
+                                    <th>No.</th>
+                                    <th>Course name</th>
+                                    <th>Duration</th>
+                                    <th>Full Marks</th>
+                                    <th>Obtained Marks</th>
+                                    <th>Percentage</th>
+                                    <th>Remarks</th>
+                                    <th>Graded By</th>
                                 </tr>
-                            )
-                        }
-                    </tbody>
-                </table>
-            </div>
+                            </thead>
+                            <tbody>
+                                {
+                                    studentGrades?.map((record, idx) =>
+                                        <tr
+                                            className="hover"
+                                            key={record?._id}>
+                                            <th>{idx + 1}</th>
+
+                                            <td>{record?.course_id?.title}</td>
+
+                                            <td>{format(new Date(record?.course_id?.start_date), "MMMM d, yyyy")} - {format(new Date(record?.course_id?.end_date), "MMMM d, yyyy")}</td>
+
+                                            <td className="text-center">{record?.full_marks}</td>
+
+                                            <td className="text-center">{record?.obtained_marks}</td>
+
+                                            <td className="text-center">{record?.percentage.toFixed(2)}%</td>
+
+                                            <td className="text-center">{record?.remarks}</td>
+
+                                            <td className="text-center">{record?.faculty_id?.first_name} {record?.faculty_id?.last_name}</td>
+                                        </tr>
+                                    )
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+
+            }
+
         </div>
     );
 };
+
+StudentGradesTab.propTypes = {
+    studentGrades: PropTypes.array,
+    studentGradesIsPending: PropTypes.bool,
+    studentGradesIsError: PropTypes.bool,
+    studentGradesError: PropTypes.func
+}
 
 export default StudentGradesTab;
